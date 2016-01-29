f48a1a2d63c6f1d74c317196d27133c5f4655715
Ayo Arum <alchemist@Invidia.local>
2016-01-29 10:08:02 -0500

    changed arit of get-pos

--- src/matrix/util.clj
+++ src/matrix/util.clj
@@ -1,22 +1,12 @@
 (ns matrix.util)
 
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (with-meta x {:row_cnt (:row_cnt (meta x))
-                :col_cnt (:col_cnt (meta x))
-                :col_names (:col_names (meta x))
-                :row_names (:row_names (meta x))}))
-
-
 (defn get-row [pos M] (let [col_cnt (:col_cnt (meta M))]
                         (quot pos col_cnt)))
 
 (defn get-col [pos M] (let [col_cnt (:col_cnt (meta M))]
                         (mod pos col_cnt)))
 
-(defn get-pos [i j M] (let [col_cnt (:col_cnt (meta M))] 
+(defn get-pos [[i j] M] (let [col_cnt (:col_cnt (meta M))] 
                         (+ (* i col_cnt) j)))
 
 (defn get-coor [pos M] (list (get-row pos M)  (get-col pos M)))
@@ -29,14 +19,13 @@
 (defn first-col [M] (take-nth (:col_cnt (meta M)) M))
 (defn last-row [M] (reverse (take (:col_cnt (meta M)) (reverse M))))
 (defn last-col [M] (reverse (take-nth (:col_cnt (meta M)) (reverse M))))
-
 (defn nth-row [n M] (take (:col_cnt (meta M)) (drop (* n (:col_cnt (meta M))) M)))
 (defn nth-col [n M] (take-nth (:col_cnt (meta M)) (drop n M)))
 
 (defn diagonal [M] (take-nth (+ 1 (:col_cnt (meta M))) M));left to right diagonal
 
 
-(defn scalar-add [scalar M]   (let [i (:row_cnt (meta M)) j (:col_cnt (meta M))]
+(defn scalar-add [scalar M] (let [i (:row_cnt (meta M)) j (:col_cnt (meta M))]
     (with-meta (map + M (repeat (* i j) scalar)){
                :row_cnt (:row_cnt (meta M))
                :col_cnt (:col_cnt (meta M))
@@ -77,7 +66,7 @@
                :col_names (:col_names (meta M))
                :row_names (:row_names (meta M))}))))
 
-;;(apply + (map * '(9 8) '(7 6)))
+;;(apply + (map * (matrix.util/nth-row i M) (matrix.util/nth-col j N)))
 
 
 
