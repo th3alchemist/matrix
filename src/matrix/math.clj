e71bc640cf3e5e650789479c0b03a4eac821c54c
Ayo Arum <alchemist@Invidia.local>
2016-01-29 14:05:00 -0500

    added math namespace

---
+++ src/matrix/math.clj
@@ -0,0 +1,46 @@
+(ns matrix.math)
+(require 'matrix.util)
+
+(defn scalar-add [scalar M] (let [i (:row_cnt (meta M)) j (:col_cnt (meta M))]
+    (with-meta (map + M (repeat (* i j) scalar)){
+               :row_cnt (:row_cnt (meta M))
+               :col_cnt (:col_cnt (meta M))
+               :col_names (:col_names (meta M))
+               :row_names (:row_names (meta M))})));;scalar addition
+
+(defn scalar-sub [scalar M] (let [i (:row_cnt (meta M)) j (:col_cnt (meta M))]
+    (with-meta (map - M (repeat (* i j) scalar)){
+               :row_cnt (:row_cnt (meta M))
+               :col_cnt (:col_cnt (meta M))
+               :col_names (:col_names (meta M))
+               :row_names (:row_names (meta M))})));;scalar subtraction
+  
+  
+(defn scalar-mul [scalar M] (let [i (:row_cnt (meta M)) j (:col_cnt (meta M))]
+    (with-meta (map * M (repeat (* i j) scalar)){
+               :row_cnt (:row_cnt (meta M))
+               :col_cnt (:col_cnt (meta M))
+               :col_names (:col_names (meta M))
+               :row_names (:row_names (meta M))})));;scalar multiplication
+
+(defn matrix-add [M N] (with-meta (map + M N){
+               :row_cnt (:row_cnt (meta M))
+               :col_cnt (:col_cnt (meta M))
+               :col_names (:col_names (meta M))
+               :row_names (:row_names (meta M))}))
+
+(defn matrix-sub [M N] (with-meta (map - M N){
+               :row_cnt (:row_cnt (meta M))
+               :col_cnt (:col_cnt (meta M))
+               :col_names (:col_names (meta M))
+               :row_names (:row_names (meta M))}))
+
+(defn matrix-mul [M N] (if true
+                         (do (with-meta (map * M N){
+               :row_cnt (:row_cnt (meta M))
+               :col_cnt (:col_cnt (meta M))
+               :col_names (:col_names (meta M))
+               :row_names (:row_names (meta M))}))))
+
+
+;;(apply + (map * (matrix.util/nth-row i M) (matrix.util/nth-col j N)))
