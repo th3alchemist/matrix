3f1a5416a9cae827a2324a1414e3b58dad682769
Ayo Arum <alchemist@Invidia.local>
2016-01-28 19:20:00 -0500

    added meta-data to scalar arithatic functions

--- src/matrix/util.clj
+++ src/matrix/util.clj
@@ -4,7 +4,10 @@
 (defn foo
   "I don't do a whole lot."
   [x]
-  (println x "Hello, World!"))
+  (with-meta x {:row_cnt (:row_cnt (meta x))
+                :col_cnt (:col_cnt (meta x))
+                :col_names (:col_names (meta x))
+                :row_names (:row_names (meta x))}))
 
 
 (defn get-row [pos M] (let [col_cnt (:col_cnt (meta M))]
@@ -30,11 +33,39 @@
 
 
 (defn scalar-add [scalar M]   (let [i (:row_cnt (meta M)) j (:col_cnt (meta M))]
-    (map + M (repeat (* i j) scalar))));;scalar addition
-;;make a lazy seq of scalar
+    (with-meta (map + M (repeat (* i j) scalar)){
+               :row_cnt (:row_cnt (meta M))
+               :col_cnt (:col_cnt (meta M))
+               :col_names (:col_names (meta M))
+               :row_names (:row_names (meta M))})));;scalar addition
+
 (defn scalar-sub [scalar M] (let [i (:row_cnt (meta M)) j (:col_cnt (meta M))]
-    (map - M (repeat (* i j) scalar))));;scalar subtraction
+    (with-meta (map - M (repeat (* i j) scalar)){
+               :row_cnt (:row_cnt (meta M))
+               :col_cnt (:col_cnt (meta M))
+               :col_names (:col_names (meta M))
+               :row_names (:row_names (meta M))})));;scalar subtraction
+  
+  
 (defn scalar-mul [scalar M] (let [i (:row_cnt (meta M)) j (:col_cnt (meta M))]
-    (map * M (repeat (* i j) scalar))));;scalar multiplication
+    (with-meta(map * M (repeat (* i j) scalar)){
+               :row_cnt (:row_cnt (meta M))
+               :col_cnt (:col_cnt (meta M))
+               :col_names (:col_names (meta M))
+               :row_names (:row_names (meta M))})));;scalar multiplication
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
