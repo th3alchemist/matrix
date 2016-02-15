0aa84198e13b3ba0e1e8ee81683a3643357d3225
Ayo Arum <alchemist@Invidia.local>
2016-02-15 17:10:53 -0500

    chaged util to core

--- src/matrix/math.clj
+++ src/matrix/math.clj
@@ -1,6 +1,6 @@
 (ns matrix.math)
-(require 'matrix.util)
-(alias 'util 'matrix.util)
+(require 'matrix.core)
+(alias 'mCore 'matrix.core)
 
 (defn scalar-add [M scalar] (let [i (:row_cnt (meta M)) j (:col_cnt (meta M))]
     (with-meta (map + M (repeat (* i j) scalar)){
@@ -36,7 +36,7 @@
                :row_names (:row_names (meta M))}))
 
 (defn matrix-mul [M N]
-  (loop [pos 0 out (with-meta (util/matrix (:row_cnt (meta N)) (:col_cnt (meta M)))
+  (loop [pos 0 out (with-meta (mCore/matrix (:row_cnt (meta N)) (:col_cnt (meta M)))
                     {:row_cnt (:row_cnt (meta M)) :col_cnt (:col_cnt (meta N)) :col_names (:col_names (meta M)) :row_names (:row_names (meta N))})]
     (if (= pos (* (:row_cnt (meta M)) (:col_cnt (meta N))))
       out
@@ -45,11 +45,11 @@
         (assoc (with-meta out {:row_cnt (:row_cnt (meta M)) :col_cnt (:col_cnt (meta N)) :col_names (:col_names (meta M)) :row_names (:row_names (meta N))})
                pos
                (apply + (map *
-                             (util/nth-row M (nth (seq (util/get-coor out pos)) 0))
-                             (util/nth-col N (nth (seq (util/get-coor out pos)) 1)))))))))
+                             (mCore/nth-row M (nth (seq (mCore/get-coor out pos)) 0))
+                             (mCore/nth-col N (nth (seq (mCore/get-coor out pos)) 1)))))))))
 
 (defn matrix-not [M]
-  (loop [pos 0 out (with-meta (util/matrix (:row_cnt (meta M))
+  (loop [pos 0 out (with-meta (mCore/matrix (:row_cnt (meta M))
                                            (:col_cnt (meta M)))
                               {:row_cnt (:row_cnt (meta M))
                                :col_cnt (:col_cnt (meta M))
@@ -62,7 +62,7 @@
         (assoc out pos (not (get M pos)))))))
 
 (defn matrix-and [M N]
- (loop [pos 0 out (with-meta (util/matrix (:row_cnt (meta M))
+ (loop [pos 0 out (with-meta (mCore/matrix (:row_cnt (meta M))
                                           (:col_cnt (meta M)))
                               {:row_cnt (:row_cnt (meta M))
                                :col_cnt (:col_cnt (meta M))
@@ -75,7 +75,7 @@
         (assoc out pos (and (get M pos) (get N pos)))))))
 
 (defn matrix-or [M N]
-  (loop [pos 0 out (with-meta (util/matrix (:row_cnt (meta M))
+  (loop [pos 0 out (with-meta (mCore/matrix (:row_cnt (meta M))
                                            (:col_cnt (meta M)))
                               {:row_cnt (:row_cnt (meta M))
                                :col_cnt (:col_cnt (meta M))
@@ -87,17 +87,14 @@
         (inc pos)
         (assoc out pos (or (get M pos) (get N pos)))))))
 
-(defn trace [M] (apply + (util/diagonal M)))
+(defn trace [M] (apply + (mCore/diagonal M)))
 
-(defn det [M] (if (and (util/square-matrix? M)
+(defn det [M] (if (and (mCore/square-matrix? M)
                        (= 4 (count M)))
-                          (+ (* (nth M 0)
+                          (- (* (nth M 0)
                                 (nth M 3))
                              (* (nth M 1)
                                 (nth M 2)))))
-
-
-
 
 
 
